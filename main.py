004d3385479b91f73db1567fba0e53011e890372
dvdplayer26 <davidl526@nycstudents.net>
2025-10-29 08:47:45 -0400

    Revise team members and function headers in main.py
    Updated team information and organized function sections.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # ==================================
 #      Python Quote Scraper
 #
-# Team: [Your Team Name]
-# Members: [List of team member names]
+# Team: Silliness
+# Members: Arshia, Pearl, Alvin, David, Svitozar (Ed), Ronny, Akib
 # ==================================
 
 single_quote = {
@@ -29,61 +29,39 @@ multi_quote = [
 
 # SECTION 1: IMPORTS
 # All team members: Add the libraries you need for your function here.
-# import requests
-# import json
-# import random
-# from bs4 import BeautifulSoup
-
+import requests
+import json
+import random
+from bs4 import BeautifulSoup
 
 # ==================================
 # SECTION 2: FUNCTION DEFINITIONS
 # ==================================
 
-# --- Function for Student A ---
+# --- Function for Akib ---
+# TODO: Put your group_introductions() function here.
+# This function should print an introduction of the group, and ask the user for inputting the date.
+# It should return the name of our group members and an input asking for the user to input the date.
+
+# --- Function for Alvin and Pearl ---
 # TODO: Put your scrape_all_quotes function here.
 # This function should scrape all quotes from the website.
 # It should return a list of quote dictionaries.
 
 
-# --- Function for Student B ---
+# --- Function for Svitozar (Ed) ---
 # TODO: Put your save_quotes_to_disk function here.
 # This function should take the list of quotes and a filename.
 # It should save the quotes to a JSON or CSV file.
 
 
-# --- Function for Student C ---
+# --- Function for David ---
 # TODO: Put your load_quotes_from_disk function here.
 # This function should take a filename.
 # If the file exists, it returns the list of quotes from the file.
 # If the file does not exist, it returns an empty list [].
 
 
-# --- Function for Student D ---
+# --- Function for Alvin and Pearl ---
 # TODO: Put your get_quotes_by_tag function here.
 # This function should take the list of quotes.
-# It asks the user for a tag and prints any matching quotes.
-
-
-# --- Function for Student E ---
-# TODO: Put your get_random_quote function here.
-# This function should take the list of quotes.
-# It picks one random quote and prints it.
-
-
-
-
-
-
-
-
-
-
-
-
-# ==================================
-# SECTION 3: MAIN PROGRAM
-# ==================================
-
-
-# Team Lead/Integrator: Write the main logic here that calls the functions.
-if __name__ == "__main__":
